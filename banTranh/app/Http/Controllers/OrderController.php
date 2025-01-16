fcd22bc38087dd96f0ca772843ace2c1698acdad
namphan06 <117923465+namphan06@users.noreply.github.com>
2025-01-16 06:18:03 +0000

    Update OrderController

--- banTranh/app/Http/Controllers/OrderController.php
+++ banTranh/app/Http/Controllers/OrderController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
     public function index(): View
     {
         $orders = Order::latest()->paginate(6); 
+
+        return view('orders.index', compact('orders'));
+    }
+
+    public function getOrderEmail(): View
+    {
+        // Lấy người dùng hiện tại
+        $user = Auth::user();
+
+        // Lọc các đơn hàng theo email của người dùng hiện tại
+        $orders = Order::where('email', $user->email)->latest()->paginate(6); 
 
         return view('orders.index', compact('orders'));
     }
