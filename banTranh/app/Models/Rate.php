d3148a929f5a7d4d4487bb5f546c5af5900ce1af
namphan06 <117923465+namphan06@users.noreply.github.com>
2025-01-27 09:24:26 +0000

    Add Rate

---
+++ banTranh/app/Models/Rate.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rate extends Model
+{
+    use HasFactory;
+
+    protected $table = 'rates';
+
+    protected $fillable = [
+        'product_id',
+        'email',
+        'stars',
+        'comment',
+        'image',
+        'rated_at',
+        
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
